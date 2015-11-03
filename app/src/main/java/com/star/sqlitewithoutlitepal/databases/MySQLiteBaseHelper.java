d40426878c04b0d7dbf0f40f38a01e89544b5c8f
crystalDf <chendong333@gmail.com>
2015-11-04 00:48:18 +0800

    Chapter 14 SQLiteWithoutLitePal onUpgrade()

--- app/src/main/java/com/star/sqlitewithoutlitepal/databases/MySQLiteBaseHelper.java
+++ app/src/main/java/com/star/sqlitewithoutlitepal/databases/MySQLiteBaseHelper.java
@@ -5,11 +5,12 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.star.sqlitewithoutlitepal.databases.MySQLiteDbSchema.CommentTable;
 import com.star.sqlitewithoutlitepal.databases.MySQLiteDbSchema.NewsTable;
 
 public class MySQLiteBaseHelper extends SQLiteOpenHelper {
 
-    private static final int VERSION = 1;
+    private static final int VERSION = 3;
     private static final String DATABASE_NAME = "demo.db";
 
     public MySQLiteBaseHelper(Context context) {
@@ -19,10 +20,18 @@ public class MySQLiteBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(NewsTable.CREATE_TABLE);
+        db.execSQL(CommentTable.CREATE_TABLE);
+        db.execSQL(CommentTable.ALTER_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        switch (oldVersion) {
+            case 1:
+                db.execSQL(CommentTable.CREATE_TABLE);
+            case 2:
+                db.execSQL(CommentTable.ALTER_TABLE);
+            default:
+        }
     }
 }
--- app/src/main/java/com/star/sqlitewithoutlitepal/databases/MySQLiteDbSchema.java
+++ app/src/main/java/com/star/sqlitewithoutlitepal/databases/MySQLiteDbSchema.java
@@ -13,7 +13,6 @@ public class MySQLiteDbSchema {
             public static final String CONTENT = "content";
             public static final String PUBLISHED_DATE = "published_date";
             public static final String COMMENT_COUNT = "comment_count";
-
         }
 
         public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
@@ -23,5 +22,25 @@ public class MySQLiteDbSchema {
                 + Cols.PUBLISHED_DATE + " INTEGER, "
                 + Cols.COMMENT_COUNT + " INTEGER"
                 + ")";
+    }
+
+    public static final class CommentTable {
+
+        public static final String TABLE_NAME = "comment";
+
+        public static final class Cols {
+            private static final String ID = "_id";
+            public static final String CONTENT = "content";
+            public static final String PUBLISHED_DATE = "published_date";
+        }
+
+        public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                + Cols.ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + Cols.CONTENT + " TEXT"
+                + ")";
+
+        public static final String ALTER_TABLE = "ALTER TABLE " + TABLE_NAME + " ADD COLUMN "
+                + Cols.PUBLISHED_DATE + " INTEGER";
+
     }
 }
